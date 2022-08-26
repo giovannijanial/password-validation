120349bec8797502d1be07320612a76bda105272
Giovanni Janial <giovannijanial@gmail.com>
2022-08-26 15:32:27 -0300

    change lowercase and uppercase validator not consider special characters

--- backend/src/modules/passwordValidator/utils/lowercaseAndUppercaseValidator.ts
+++ backend/src/modules/passwordValidator/utils/lowercaseAndUppercaseValidator.ts
@@ -1,15 +1,22 @@
 class LowercaseAndUppercaseValidator {
 	execute(password: string, objResult: IResult): IResult {
+		const SPECIAL_CHARACTERS = "!@#$%^&*()_+=-{}[];',./:<>?";
 		let hasLower = false;
 		let hasUpper = false;
 		const arrPassword = password.split("");
 
 		arrPassword.forEach((character) => {
-			if (character.toLowerCase() === character) {
+			if (
+				character.toLowerCase() === character &&
+				!SPECIAL_CHARACTERS.includes(character)
+			) {
 				hasLower = true;
 			}
 
-			if (character.toUpperCase() === character) {
+			if (
+				character.toUpperCase() === character &&
+				!SPECIAL_CHARACTERS.includes(character)
+			) {
 				hasUpper = true;
 			}
 		});
